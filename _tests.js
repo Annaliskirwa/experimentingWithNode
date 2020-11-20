c29779de29e447682a544e693128b39d0415cb01
Annaliskirwa <annaliskirwa@gmail.com>
2020-11-20 15:31:00 +0300

    adding requestss

--- _tests.js
+++ _tests.js
@@ -1,7 +1,11 @@
 var http = require("http")
 
 var ourApp = http.createServer(function (req, res){
-    console.log(req.url)
-    res.end("Hello, Welcome to my website")
+    if (req.url == "/"){
+        res.end ("Hello and welcome to my website")
+    }
+    if (req.url == "/about"){
+        res.end ("Thank you for your interest")
+    }
 })
 ourApp.listen(3000) 
