2c9d16a7fd3f069a6e3e1b3fb042998e00d9da70
Annaliskirwa <annaliskirwa@gmail.com>
2020-11-23 13:59:25 +0300

    using literals

--- __tests.js
+++ __tests.js
@@ -3,9 +3,9 @@ var ourApp = express()
 
 ourApp.get('/', function(req, res){
 res.send(`
-<form action="/answer" method="POST" > 
+<form action = "/answer" method = "POST" > 
 <p> What color is the sky on a clear and sunny day?</p>
-<input name="skyColor" autocomplete ="off">
+<input name = "skyColor" autocomplete = "off">
 <button> Submit answer </button>
 </form>
 `)
