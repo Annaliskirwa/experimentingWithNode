a42a7fdb671a760fb33387d3125023e103df097f
Annaliskirwa <annaliskirwa@gmail.com>
2020-11-23 12:55:19 +0300

    fifth commit

--- __tests.js
+++ __tests.js
@@ -1,11 +1,11 @@
-let express = require ("express")
-let ourApp = express()
+var express = require ("express")
+var ourApp = express()
 
 ourApp.get('/', function(req, res){
-res.end(`
-<form action = "/answer" method = "POST" > 
+res.send(`
+<form action="/answer" method="POST" > 
 <p> What color is the sky on a clear and sunny day?</p>
-<input name = "skyColor" autocomplete = "off">
+<input name="skyColor" autocomplete ="off">
 <button> Submit answer </button>
 </form>
 `)
