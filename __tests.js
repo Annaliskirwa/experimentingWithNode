dfb9dc25adf87a8ec60cf4a44374647ba22d199b
Annaliskirwa <annaliskirwa@gmail.com>
2020-11-23 15:24:28 +0300

    dealing with user submitted data

--- __tests.js
+++ __tests.js
@@ -1,6 +1,8 @@
 var express = require ("express")
 var ourApp = express()
 
+ourApp.use(express.urlencoded({extended: false}))
+
 ourApp.get('/', function(req, res){
 res.send(`
 <form action = "/answer" method = "POST" > 
@@ -12,7 +14,18 @@ res.send(`
 })
 
 ourApp.post('/answer', function(req, res){
-    res.send("Thankyou for submitting the form")
+   if (req.body.skyColor == "blue"){
+       res.send(`
+       <p> Congrats, that is the correct answer</p>
+       <a href = "/"> Back to homepage</a>
+       `)
+   }
+   else{
+    res.send(`
+    <p> Sorry, that is the incorrect answer</p>
+    <a href = "/"> Back to homepage</a>
+    `)
+   }
 })
 
 ourApp.get('/answer', function(req, res){
